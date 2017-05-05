7c24777b5552a43fc29e10f76a74ac630641145e
ArnaudStigliani <arnaud.stigliani@cea.fr>
2017-05-05 17:11:33 +0200

    first commit

---
+++ auto/lab_meeting_06_03_2017.el
@@ -0,0 +1,24 @@
+(TeX-add-style-hook
+ "lab_meeting_06_03_2017"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("adjustbox" "export") ("inputenc" "utf8")))
+   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "beamer"
+    "beamer10"
+    "adjustbox"
+    "tikz"
+    "animate"
+    "inputenc"
+    "textcomp"))
+ :latex)
+
